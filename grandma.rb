f0a286f92737cbf6f8ea134a43d29eacd51c6922
SarahHartwick <sarah@surfsetfitness.com>
2016-06-21 09:26:55 -0400

    Grandma Warmup Diagnostic
    Complete question without Bonus

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,18 @@
 require 'pry'
+prompt = 'SPEAK: '
+puts 'Speak to Grandma'
+print prompt
+
+while user_input = gets.chomp # loop while getting user input
+  if user_input == 'BYE'
+    `say bye`
+    print 'BYE!'
+    break
+  elsif user_input == user_input.upcase
+    `say NO, NOT SINCE #{(1930 + rand(20)).to_s}!`
+    print prompt
+  else
+    `say HUH?! SPEAK UP, SONNY!`
+    print prompt
+  end
+end
